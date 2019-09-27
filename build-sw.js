72a383952f92028d0919afd70f9547ba1680e8b8
wucl2 <32004624+wucl2@users.noreply.github.com>
2019-09-27 17:53:34 +0800

    Update build-sw.js

--- build-sw.js
+++ build-sw.js
@@ -16,26 +16,14 @@ self.addEventListener("install", function (event) {
 });
 
 // If any fetch fails, it will show the offline page.
-self.addEventListener("fetch", function (event) {
-  if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    fetch(event.request).catch(function (error) {
-      // The following validates that the request was for a navigation to a new document
-      if (
-        event.request.destination !== "document" ||
-        event.request.mode !== "navigate"
-      ) {
-        return;
-      }
-
-      console.error("[PWA Builder] Network request Failed. Serving offline page " + error);
-      return caches.open(CACHE).then(function (cache) {
-        return cache.match(offlineFallbackPage);
-      });
+self.addEventListener('fetch', e => {
+  console.log(caches)
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res
     })
-  );
-});
+  )
+})
 
 // This is an event that can be fired from your page to tell the SW to update the offline page
 self.addEventListener("refreshOffline", function () {
